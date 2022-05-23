671e375eba9ca309f32a125fd54e77c9cf6fc1b9
ritiksingh16 <99740228+ritiksingh16@users.noreply.github.com>
2022-05-24 01:01:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Div1 from './conponents/Div1'
+
+function App() {
+  return (
+    <div className="App">
+      <Div1></Div1>
+
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/conponents/Div1.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Div1 = () =>{
+    return(
+        <div className='graybox'>
+            <div>LOGOBAKERY</div>
+
+            <div className='middlebox'>
+                <div>Services</div>
+                <div>Projects</div>
+                <div>About</div>
+            </div>
+
+
+
+            <div className='third'>
+                <button>Contact</button>
+            </div>
+        </div>
+    )
+}
+
+export default Div1;
